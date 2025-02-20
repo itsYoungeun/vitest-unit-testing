3445f65db9160a840ac7c27ead9ac5b360047038
Peter <youngeunpc@yahoo.com>
2025-02-19 22:36:48 -0300

    Implemented tests for fizzBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,22 +1,7 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { max, fizzBuzz } from "../src/intro";
 
 describe('max', () => {
-    // AAA
-    // Arrange: Turn on the TV
-    // Act: Press the power button
-    // Assert: Verify TV is off
-
-    // // Arrange
-    // const a = 2;
-    // const b = 1;
-
-    // // Act
-    // const result = max(a, b);
-
-    // // Assert
-    // expect(result).toBe(2);
-
     it('should return the first argument if it is greater', () => {
         expect(max(2, 1)).toBe(2);
     })
@@ -27,5 +12,23 @@ describe('max', () => {
 
     it('should return the first argument if arguments are equal', () => {
         expect(max(1, 1)).toBe(1);
+    })
+})
+
+describe('fizzBuzz', () => {
+    it('should return FizzBuzz if arg is divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+
+    it('should return Fizz if arg is only divisible by 3', () => {
+        expect(fizzBuzz(12)).toBe('Fizz');
+    })
+
+    it('should return Buzz if arg is only divisible by 5', () => {
+        expect(fizzBuzz(10)).toBe('Buzz');
+    })
+
+    it('should return arg as a string if not divisible by 5 or 3', () => {
+        expect(fizzBuzz(8)).toBe('8');
     })
 })
